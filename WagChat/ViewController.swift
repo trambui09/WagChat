81de7059cb56f651840ef2e28a69245dd9502c49
Tram Bui <pbui17@cmc.edu>
2021-01-26 11:04:36 -0600

    Initial Commit

---
+++ WagChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WagChat
+//
+//  Created by Tram Bui on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
