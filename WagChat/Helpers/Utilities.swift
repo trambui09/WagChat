2fb1ace3cf25bf03d79a869882a8f195e13c131e
Tram Bui <pbui17@cmc.edu>
2021-01-26 21:26:42 -0600

    made a new utilities file

--- WagChat/Helpers/Utilities.swift
+++ WagChat/Helpers/Utilities.swift
@@ -10,17 +10,44 @@ import UIKit
 
 class Utilities {
     
-    static func styleTextField(_ textField:UITextField) {
+    static func styleTextField(_ textfield:UITextField) {
+        
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
+        
+        bottomLine.backgroundColor = UIColor.init(red: 1.0, green: 0.5, blue: 0.0, alpha: 1).cgColor
+        
+        // Remove border on text field
+        textfield.borderStyle = .none
+        
+        // Add the line to the text field
+        textfield.layer.addSublayer(bottomLine)
         
     }
     
     static func styleFilledButton(_ button:UIButton) {
-        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, aplha: 1)
-        button.layer.cornerRadius = 20.0
+        
+        // Filled rounded corner style
+        button.backgroundColor = UIColor.init(red: 1.0, green: 0.5, blue: 0.0, alpha: 1)
+        button.layer.cornerRadius = 25.0
         button.tintColor = UIColor.white
     }
     
-    static func isPasswordValid(_ password: String) -> Bool {
-        let passwordTest = NSPredicate(format: <#T##String#>, <#T##args: CVarArg...##CVarArg#>)
+    static func styleHollowButton(_ button:UIButton) {
+        
+        // Hollow rounded corner style
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.black
     }
+    
+    static func isPasswordValid(_ password : String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
 }
