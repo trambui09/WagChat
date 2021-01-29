55a8677292e7b80c57db53a6797ac6516ee03502
MonaRahmani <monarahmanimehr@gmail.com>
2021-01-29 13:00:13 -0800

    added comments

--- WagChat/View Controllers/chat.swift
+++ WagChat/View Controllers/chat.swift
@@ -3,14 +3,17 @@
 //  WagChat
 //
 //  Created by Ada on 1/29/21.
+
 // store chat object in this file
 
 import UIKit
 
+// creat struct with 2 variable
 struct Chat {
     
     var users: [String]
     
+   // returning dictionary of chat object
     var dictionary: [String: Any] {
         return [
             "users": users
@@ -18,9 +21,12 @@ struct Chat {
     }
 }
 
+// add new functionality - add initializer
 extension Chat {
     
     init?(dictionary: [String:Any]) {
+        // add guard to make sure it's not nil
+        // initialize the class with users
         guard let chatUsers = dictionary["users"] as? [String] else {return nil}
         self.init(users: chatUsers)
     }
