7869ecf5eca59a87424dfcc9cdf6f07c60d78926
MonaRahmani <monarahmanimehr@gmail.com>
2021-01-29 12:35:42 -0800

    chat object added to the chat file

---
+++ WagChat/View Controllers/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  WagChat
+//
+//  Created by Ada on 1/29/21.
+//
+
+import UIKit
---
+++ WagChat/View Controllers/chat.swift
@@ -0,0 +1,28 @@
+//
+//  chat.swift
+//  WagChat
+//
+//  Created by Ada on 1/29/21.
+// store chat object in this file
+
+import UIKit
+
+struct Chat {
+    
+    var users: [String]
+    
+    var dictionary: [String: Any] {
+        return [
+            "users": users
+        ]
+    }
+}
+
+extension Chat {
+    
+    init?(dictionary: [String:Any]) {
+        guard let chatUsers = dictionary["users"] as? [String] else {return nil}
+        self.init(users: chatUsers)
+    }
+    
+}
