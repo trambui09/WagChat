0b116db515d202d6dfa8bc4bead0100cbe55c2fd
Tram Bui <pbui17@cmc.edu>
2021-02-04 15:27:21 -0600

    profile details shows on screen

--- WagChat/View Controllers/ProfileDetailsViewController.swift
+++ WagChat/View Controllers/ProfileDetailsViewController.swift
@@ -5,6 +5,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
+import FirebaseFirestore
 
 class ProfileDetailsViewController: UIViewController {
 
@@ -26,8 +29,32 @@ class ProfileDetailsViewController: UIViewController {
         title = "Selected User Profile"
 
         // Do any additional setup after loading the view.
+        populateProfileDetails()
     }
     
+    func populateProfileDetails() {
+        
+//        usernameLabel.text = "Tram is great"
+        let db = Firestore.firestore()
+        let docRef = db.collection("users").document(selectedUserUID!)
+
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+//                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+//                print("Document data: \(dataDescription)")
+                // need a check if the fields are nil or not
+                if document.data()?["dogInfo"] != nil && document.data()?["location"] != nil && document.data()?["about"] != nil &&  document.data()?["username"] != nil {
+                    self.usernameLabel.text = document.data()?["username"]! as? String
+                    self.locationLabel.text = "Location: \(document.data()?["location"]! as? String ?? "No Location")"
+                    self.dogLabel.text = "Dog Information: \(document.data()?["dogInfo"]! as? String ?? "No Dog Information")"
+                    self.aboutLabel.text = "About: \(document.data()?["about"]! as? String ?? "No About Section")"
+                }
+
+            } else {
+                print("Document does not exist")
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -38,5 +65,8 @@ class ProfileDetailsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func messageButtonTapped(_ sender: Any) {
+        print("message selected user")
+    }
+    
 }
